9e80fa1fa37a29ab9e541492879e663bf9a2ae72
Shahin Alom <shahinalomshuvo94@gmail.com>
2024-01-04 14:08:43 +0600

    fixed duplicate submission issue

--- src/api/contestSubmission/createContestSubmission.js
+++ src/api/contestSubmission/createContestSubmission.js
@@ -2,8 +2,22 @@ const ContestSubmissionModel = require("../../models/contestSubmission");
 
 const createContestSubmission = async (req, res) => {
   try {
-    const { email, name, submittedTask, contestId, contestName, image } =
-      req.body;
+    const { email, contestId } = req.body;
+
+    // Check if a submission already exists for the user and contest
+    const existingSubmission = await ContestSubmissionModel.findOne({
+      email,
+      contestId,
+    });
+
+    // If a submission exists, prevent creating a new one
+    if (existingSubmission) {
+      return res
+        .status(400)
+        .json({ message: "You have already submitted for this contest." });
+    }
+
+    const { name, submittedTask, contestName, image } = req.body;
     const result = await ContestSubmissionModel.create({
       email,
       name,
@@ -12,7 +26,7 @@ const createContestSubmission = async (req, res) => {
       contestId,
       contestName,
     });
-    res.json(result);
+    res.status(200).json(result);
   } catch (err) {
     console.error(err);
     res.status(500).send("Internal Server Error");
